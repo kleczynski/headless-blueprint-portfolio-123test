a315f537877967c5a6b9acf229db1baa843bae9d
Kacper Łęczyński <kacper.leczynski@wpengine.com>
2025-10-15 14:25:28 +0200

    test: bad commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,5 @@
 import '../faust.config';
 import React from 'react';
-import { useRouter } from 'next/router';
 import { FaustProvider } from '@faustwp/core';
 import 'normalize.css/normalize.css';
 import '../styles/main.scss';
